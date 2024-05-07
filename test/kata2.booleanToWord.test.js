034fc54b7b7be2507605d04330083a98b2f7b4fa
Erkan Gazi <erkangazi20@gmail.com>
2024-05-08 01:37:00 +0300

    Boolean to Word exercise complete

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,11 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-  // how do we create specs again???
+  it("Returns Yes if true", () => {
+    expect(booleanToWord('true')).toBe('Yes')
+  });
+
+  it("Returns No if  false", () => {
+    expect(booleanToWord('false')).toBe('No')
+  });
 });

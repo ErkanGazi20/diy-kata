7870b215e970d43dd4f090a5f9d0dbe534cd0e2c
Erkan Gazi <erkangazi20@gmail.com>
2024-05-09 13:50:56 +0300

    Completed getEmployerRole

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,6 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+    const employee = employees.find(emp => emp.name === employeeName);
+    return employee ? employee.role :'Employee not found';
+};
 
 module.exports = getEmployerRole;
